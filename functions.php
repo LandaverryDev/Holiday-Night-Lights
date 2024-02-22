2ce0be6f2cef0c2d95c82aa7ec900c36e70e396d
mr.obayed <95936785+mr-obayed@users.noreply.github.com>
2024-02-22 08:32:32 +0600

    Sohag initial Commit

--- functions.php
+++ functions.php
@@ -92,3 +92,4 @@ function understrap_child_customize_controls_js() {
 	);
 }
 add_action( 'customize_controls_enqueue_scripts', 'understrap_child_customize_controls_js' );
+/* This is a test comment*/
